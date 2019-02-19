1dbfe368e9a60e97e7bf85740b5ca0a3e9fb03ec
fanxing <617332635@qq.com>
2019-02-19 16:01:44 +0800

    添加

---
+++ spring-boot-sample-xml/src/main/java/sample/xml/SampleSpringXmlApplication.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.xml;
+
+import java.util.Collections;
+
+import sample.xml.service.HelloWorldService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+public class SampleSpringXmlApplication implements CommandLineRunner {
+
+	private static final String CONTEXT_XML = "classpath:/META-INF/application-context.xml";
+
+	@Autowired
+	private HelloWorldService helloWorldService;
+
+	@Override
+	public void run(String... args) {
+		System.out.println(this.helloWorldService.getHelloMessage());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication();
+		//可以加载自己的配置文件，也就是说spring boot可以有两个配置源
+		application.setSources(Collections.singleton(CONTEXT_XML));
+		application.run(args);
+	}
+
+}
